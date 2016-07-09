93d63fa6a4f8e014b7192f9003fe2df72997bf9b
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2016-07-09 22:35:33 +0200

    Update Heap sort algorithm to use min heap

--- src/main/java/org/home/sort/HeapSort.java
+++ src/main/java/org/home/sort/HeapSort.java
@@ -1,16 +1,14 @@
 package org.home.sort;
 
+import static java.util.stream.IntStream.range;
+
 public class HeapSort extends SortAlgorithm {
     @Override
     public int[] sort(int[] unsortedArray) {
         buildHeap(unsortedArray);
 
-        int lastElementIndex = unsortedArray.length - 1;
-
-        for (int i = lastElementIndex; i > 0; i--) {
-            swap(unsortedArray, 0, i);
-            maxHeap(unsortedArray, 0, --lastElementIndex);
-        }
+        range(0, unsortedArray.length)
+                .forEach(currentElementIndex -> minHeap(unsortedArray, currentElementIndex, unsortedArray.length - 1));
 
         return unsortedArray;
     }
@@ -18,25 +16,25 @@ public class HeapSort extends SortAlgorithm {
     private void buildHeap(int[] array) {
         int amountOfElements = array.length - 1;
         for (int i = amountOfElements / 2; i >= 0; i--) {
-            maxHeap(array, i, amountOfElements);
+            minHeap(array, i, amountOfElements);
         }
     }
 
-    private void maxHeap(int[] array, int parentElementIndex, int lastElementIndex) {
+    private void minHeap(int[] array, int parentElementIndex, int lastElementIndex) {
         int leftChildElementIndex = 2 * parentElementIndex;
         int rightChildElementIndex = leftChildElementIndex + 1;
 
-        int largestElementIndex = leftChildElementIndex <= lastElementIndex && array[leftChildElementIndex] > array[parentElementIndex]
+        int largestElementIndex = leftChildElementIndex <= lastElementIndex && array[leftChildElementIndex] < array[parentElementIndex]
                 ? leftChildElementIndex
                 : parentElementIndex;
 
-        if (rightChildElementIndex <= lastElementIndex && array[rightChildElementIndex] > array[largestElementIndex]) {
+        if (rightChildElementIndex <= lastElementIndex && array[rightChildElementIndex] < array[largestElementIndex]) {
             largestElementIndex = rightChildElementIndex;
         }
 
         if (largestElementIndex != parentElementIndex) {
             swap(array, parentElementIndex, largestElementIndex);
-            maxHeap(array, largestElementIndex, lastElementIndex);
+            minHeap(array, largestElementIndex, lastElementIndex);
         }
     }
 }
